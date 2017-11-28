6369b2ec42bbc4074206be4d43ac968ea746f430
2rze <31217614+2rze@users.noreply.github.com>
2017-11-28 09:29:00 -0800

    Initial commit

---
+++ Notes.py
@@ -0,0 +1,32 @@
+#print("Hello, world!")
+
+#Forever
+
+
+# Two plus two is four, minus one that's three, quick maths
+
+print("2 + 2 is 4")
+tpf = 2 + 2
+print(tpf)
+print("minus 1 that's 3")
+qm = tpf - 1
+print(qm)
+print("quick maths")
+
+print(13%5)
+# "%" is a modulus
+
+car_name = "The Wiebe Moile"
+car_type = "BMW"
+car_cylinders = 8
+car_mpg = 5000.9
+
+print("I have a car called %s. It's a %s." % (car_name, car_type))
+#Watch the order
+
+print("memes")
+response = input(">_")
+print("yah i like %s too!111" % response)
+
+age = input("How old are you little boye?")
+print("%s! That's really old. You belong in a retirement home." % age)
